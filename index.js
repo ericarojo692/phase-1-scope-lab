094e8724c92c3d002db6898f576ef92c1152b785
Erica Rojo <ericarojo692@yahoo.com>
2021-08-11 20:51:32 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const LeastFavoriteCustomer = 'Erica'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = 'Sam'
+}
+
+
+
+
+
+
+
+
